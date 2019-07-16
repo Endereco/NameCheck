3317e65c8ad113caf734e0962168e32ba60ce5fa
Ilja <info@ilja-weber.de>
2019-07-16 13:00:09 +0200

    feat: new features and refactoring

--- NameCheck.js
+++ NameCheck.js
@@ -1,7 +1,7 @@
 /**
  * Endereco SDK.
  *
- * @author Ilja Weber <ilja.weber@mobilemjo.de>
+ * @author Ilja Weber <ilja@endereco.de>
  * @copyright 2019 mobilemojo – Apps & eCommerce UG (haftungsbeschränkt) & Co. KG
  * {@link https://endereco.de}
  */
@@ -9,9 +9,6 @@ function NameCheck(config) {
 
     var $self  = this;
     this.mapping = config.mapping;
-    this.isSet = false;
-    this.tid = 'not_set';
-    this.config = config;
     this.gender = 'X';
     this.requestBody = {
         "jsonrpc": "2.0",
@@ -20,34 +17,24 @@ function NameCheck(config) {
         "params": {
             "name": ""
         }
-    }
+    };
+    this.defaultConfig = {
+        'useWatcher': true,
+        'tid': 'not_set'
+    };
+    this.fieldsAreSet = false;
+    this.dirty = false;
+    this.config = Object.assign(this.defaultConfig, config);
     this.connector = new XMLHttpRequest();
 
-    // Observe the website in a loop.
-    setInterval( function() {
-        var isNowSet = false;
-        if(
-            (null !== document.querySelector(config.inputSelector))
-            && (null !== document.querySelector(config.salutationElement))
-        ) {
-            isNowSet = true;
-        }
-
-        if (!$self.isSet && isNowSet) {
-            $self.init();
-        } else if($self.isSet && !isNowSet) {
-            $self.isSet = false;
-        }
-    }, 300);
 
     this.init = function() {
-        console.log('Initiate NameCheck');
-        $self.inputElement = document.querySelector(config.inputSelector);
-        $self.salutationElement = document.querySelector(config.salutationElement);
-
-        // Set mark
-        $self.inputElement.setAttribute('data-service', 'nameCheck');
-        $self.inputElement.setAttribute('data-status', 'instantiated');
+        try {
+            $self.inputElement = document.querySelector($self.config.inputSelector);
+            $self.salutationElement = document.querySelector($self.config.salutationElement);
+        } catch(e) {
+            console.log('Could not initiate NameCheck because of error: ', e);
+        }
 
         // Disable browser autocomplete
         if (this.isChrome()) {
@@ -56,19 +43,19 @@ function NameCheck(config) {
             this.inputElement.setAttribute('autocomplete', 'off' );
         }
 
-        //// Rendering
-        // Generate TID
-        if (window.accounting) {
-            $self.tid = window.accounting.generateTID();
+        // Generate TID if accounting service is set.
+        if (window.accounting && ('not_set' === $self.config.tid)) {
+            $self.config.tid = window.accounting.generateTID();
         }
 
         $self.inputElement.addEventListener('change', function() {
-            $this = this;
-
-            value = $this.value.trim();
             var result = [],
                 separators = [' ', '-', '–', '_', '.'],
-                upperCase = true;
+                upperCase = true,
+                newName = '',
+                value;
+            $this = this;
+            value = $this.value.trim();
 
             for(var i = 0; i < value.length; i++)
             {
@@ -82,48 +69,139 @@ function NameCheck(config) {
             newName = result.replace(/\s{2,}/g, ' ').trim();
 
             $this.value = newName;
-
-            $self.recheck();
+            $self.checkSalutation().then( function($data) {
+                $self.resetStatus($data);
+            });
         });
 
         $self.salutationElement.addEventListener('change', function() {
-            $self.recheck();
-        })
+            $self.checkSalutation().then( function($data) {
+                $self.resetStatus($data);
+            });
+        });
 
-        $self.isSet = true;
+        $self.dirty = false;
+        console.log('NameCheck initiated');
     }
 
-    //// Functions
-    this.checkSalutation = function() {
+    this.resetStatus = function($data) {
+        var event;
+        $self.gender = $data.result.gender;
 
         if ('' === $self.salutationElement.value) {
-            event = new Event('endereco.clean');
+            event = $self.createEvent('endereco.clean');
             $self.inputElement.dispatchEvent(event);
             return;
         }
 
         if ('M' === $self.gender) {
             if($self.mapping[$self.gender] !== $self.salutationElement.value) {
-                event = new Event('endereco.check');
+                event = $self.createEvent('endereco.check');
                 $self.inputElement.dispatchEvent(event);
             } else {
-                event = new Event('endereco.valid');
+                event = $self.createEvent('endereco.valid');
                 $self.inputElement.dispatchEvent(event);
             }
         } else if ('F' === $self.gender) {
             if($self.mapping[$self.gender] !== $self.salutationElement.value) {
-                event = new Event('endereco.check');
+                event = $self.createEvent('endereco.check');
                 $self.inputElement.dispatchEvent(event);
             } else {
-                event = new Event('endereco.valid');
+                event = $self.createEvent('endereco.valid');
                 $self.inputElement.dispatchEvent(event);
             }
         } else if ('N' === $self.gender) {
-            event = new Event('endereco.valid');
+            event = $self.createEvent('endereco.valid');
             $self.inputElement.dispatchEvent(event);
         } else {
-            event = new Event('endereco.clean');
+            event = $self.createEvent('endereco.clean');
             $self.inputElement.dispatchEvent(event);
+        }
+    }
+
+    //// Functions
+    this.checkSalutation = function() {
+        return new Promise( function(resolve, reject) {
+
+            $self.connector.onreadystatechange = function() {
+                var $data = {};
+                if(4 === $self.connector.readyState) {
+                    if ($self.connector.responseText && '' !== $self.connector.responseText) {
+                        try {
+                            $data = JSON.parse($self.connector.responseText);
+                        } catch(e) {
+                            console.log('Could not parse JSON', e);
+                            reject($data);
+                        }
+
+                        if ($data.result) {
+                            resolve($data);
+                        } else {
+                            reject($data);
+                        }
+                    } else {
+                        reject($data);
+                    }
+                }
+            }
+
+            $self.requestBody.params.name = $self.inputElement.value;
+            $self.connector.open('POST', $self.config.endpoint, true);
+            $self.connector.setRequestHeader("Content-type", "application/json");
+            $self.connector.setRequestHeader("X-Auth-Key", $self.config.apiKey);
+            $self.connector.setRequestHeader("X-Transaction-Id", $self.config.tid);
+            $self.connector.setRequestHeader("X-Transaction-Referer", window.location.href);
+
+            $self.connector.send(JSON.stringify($self.requestBody));
+        });
+    }
+
+    // Check if the browser is chrome
+    this.isChrome = function() {
+        return /chrom(e|ium)/.test( navigator.userAgent.toLowerCase( ) );
+    }
+
+    /**
+     * Helper that creates an event that is compatible with IE 11.
+     *
+     * @param eventName
+     * @returns {Event}
+     */
+    this.createEvent = function(eventName) {
+        var event;
+        if(typeof(Event) === 'function') {
+            event = new Event(eventName);
+        }else{
+            event = document.createEvent('Event');
+            event.initEvent(eventName, true, true);
+        }
+        return event;
+    }
+
+    /**
+     * Helper function to update existing config, overwriting existing fields.
+     *
+     * @param newConfig
+     */
+    this.updateConfig = function(newConfig) {
+        $self.config = Object.assign($self.config, newConfig);
+    }
+
+    /**
+     * Checks if fields are set.
+     */
+    this.checkIfFieldsAreSet = function() {
+        var areFieldsSet = false;
+        if((null !== document.querySelector(config.inputSelector))
+            && (null !== document.querySelector(config.salutationElement))) {
+            areFieldsSet = true;
+        }
+
+        if (!$self.fieldsAreSet && areFieldsSet) {
+            $self.dirty = true;
+            $self.fieldsAreSet = true;
+        } else if($self.fieldsAreSet && !areFieldsSet) {
+            $self.fieldsAreSet = false;
         }
     }
 
@@ -132,36 +210,15 @@ function NameCheck(config) {
         return /chrom(e|ium)/.test( navigator.userAgent.toLowerCase( ) );
     }
 
+    // Service loop.
+    setInterval( function() {
 
-    //// DOM modifications
-
-    this.recheck = function() {
-        $self.requestBody.params.name = $self.inputElement.value;
-        $self.connector.abort();
-        $self.inputElement.setAttribute('data-status', 'loading');
-        $self.connector.open('POST', $self.config.endpoint, true);
-        $self.connector.setRequestHeader("Content-type", "application/json");
-        $self.connector.setRequestHeader("X-Auth-Key", $self.config.apiKey);
-        $self.connector.setRequestHeader("X-Transaction-Id", $self.tid);
-        $self.connector.setRequestHeader("X-Transaction-Referer", window.location.href);
-
-        $self.connector.send(JSON.stringify($self.requestBody));
-    }
-
-
-
-
-    // On data receive
-    this.connector.onreadystatechange = function() {
-        if(4 === $self.connector.readyState) {
-            if ($self.connector.responseText && '' !== $self.connector.responseText) {
-                $data = JSON.parse($self.connector.responseText);
-                if (undefined !== $data.result) {
-                    $self.gender = $data.result.gender;
-                    $self.checkSalutation();
-                }
-            }
+        if ($self.config.useWatcher) {
+            $self.checkIfFieldsAreSet();
         }
 
-    }
+        if ($self.dirty) {
+            $self.init();
+        }
+    }, 300);
 }
